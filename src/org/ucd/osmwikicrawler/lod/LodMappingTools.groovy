77dfab2875c4ab7b1329136d648428b2e1c0b3bd
Andrea Ballatore <andrea.ballatore@gmail.com>
2012-10-08 17:34:03 +0100

    added LodMappingTools

---
+++ src/org/ucd/osmwikicrawler/lod/LodMappingTools.groovy
@@ -0,0 +1,37 @@
+/*
+This file is part of the OSM Wiki Crawler.
+
+   The OSM Wiki Crawler is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   The OSM Wiki Crawler is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with the OSM Wiki Crawler.  If not, see <http://www.gnu.org/licenses/>.
+   
+   Author: Andrea Ballatore
+   Project home page: http://gforge.ucd.ie/projects/osm-similarity/
+*/
+package org.ucd.osmwikicrawler.lod
+
+import org.apache.log4j.Logger
+
+import com.hp.hpl.jena.query.ResultSetRewindable;
+
+/**
+ * 
+ * @author Andrea Ballatore
+ *
+ */
+class LodMappingTools {
+	static def log = Logger.getLogger(LodMappingTools.class)
+	
+	static def testMethod(){
+		
+	}
+}
